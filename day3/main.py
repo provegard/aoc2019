da4a1863f25bb65acfc66e8aa4a7633e71e86e7e
Per Rovegård <per@rovegard.se>
2019-12-03 06:59:06 +0100

    Day 3 part 2

--- day3/main.py
+++ day3/main.py
@@ -52,7 +52,23 @@ def coordsFor(wire):
             y += d[1]
             coords.add((x, y))
     return coords
-        
+
+def dictCoordsFor(wire):
+    parts = wire.split(",")
+    x = 0
+    y = 0
+    result = {}
+    stepsTaken = 0
+    for part in parts:
+        direction = part[0]
+        steps = int(part[1:])
+        d = delta(direction)
+        for _ in range(0, steps):
+            x += d[0]
+            y += d[1]
+            stepsTaken += 1
+            result[(x, y)] = stepsTaken
+    return result
 
 def findIntersect(wires):
     """
@@ -68,13 +84,39 @@ def findIntersect(wires):
     for i in range(1, len(allSets)):
         baseSet.intersection_update(allSets[i])
     central = (0, 0)
-    #baseSet.remove(central)
     distances = list(map(lambda c: manhattan(central, c), baseSet))
     return min(distances)
+
+def sumSteps(allDicts, c):
+    res = 0
+    for d in allDicts:
+        res += d[c]
+    return res
+
+def findSteps(wires):
+    """
+    >>> findSteps(["R8,U5,L5,D3", "U7,R6,D4,L4"])
+    30
+    >>> findSteps(["R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83"])
+    610
+    >>> findSteps(["R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51", "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"])
+    410
+    """
+    allDicts = list(map(lambda w: dictCoordsFor(w), wires))
+    baseDict = allDicts[0]
+    for i in range(1, len(allDicts)):
+        otherDict = allDicts[i]
+        for k in list(baseDict.keys()):
+            if not k in otherDict:
+                del baseDict[k]
+    
+    keys = list(baseDict.keys())
+    sums = list(map(lambda c: sumSteps(allDicts, c), keys))
+    return min(sums)
 
 #if __name__ == "__main__":
 #    import doctest
 #    doctest.testmod()
 if __name__ == "__main__":
     wires = readWires()
-    print(findIntersect(wires))
+    print(findSteps(wires))
