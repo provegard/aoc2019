5310abb8d66540f46eff3118b36b6f77342196f4
Per Rovegård <per@rovegard.se>
2019-12-04 06:19:18 +0100

    Day 4 part 2

--- day4/main.py
+++ day4/main.py
@@ -25,17 +25,47 @@ def valid(a):
         last = current
     return foundDbl
 
+def valid2(a):
+    """
+    >>> valid2(112233)
+    True
+    >>> valid2(123444)
+    False
+    >>> valid2(111122)
+    True
+    >>> valid2(111112)
+    False
+    >>> valid2(223456)
+    True
+    """
+    last = a % 10
+    a = a // 10
+    same = 0
+    foundDbl = False
+    while a > 0:
+        current = a % 10
+        if current > last:
+            return False
+        if current == last:
+            same += 1
+        elif not foundDbl:
+            foundDbl = same == 1
+            same = 0
+        a = a // 10
+        last = current
+    if not foundDbl:
+        foundDbl = same == 1
+    return foundDbl
 
 def test(a, b):
     count = 0
     for x in range(a, b+1):
-        if valid(x):
+        if valid2(x):
             count += 1
     return count
 
 
-# if __name__ == "__main__":
-#     import doctest
-#     doctest.testmod()
 if __name__ == "__main__":
+    #import doctest
+    #doctest.testmod()
     print(test(347312, 805915))
