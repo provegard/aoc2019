8a6c95872ae562a9ab00329a784539a3c3ba7d52
Per Rovegård <per@rovegard.se>
2019-12-08 22:36:36 +0100

    Day 8 part 2

--- day8/main.py
+++ day8/main.py
@@ -1,14 +1,49 @@
+from itertools import dropwhile
+
 def readInput():
     with open("input") as f:
         for line in f.readlines():
             return line
 
+def chunks(iterable, n):
+    return [iterable[i:i+n] for i in range(0, len(iterable), n)]
+
 def toLayers(digits, width, height):
-    n = width * height
-    return [digits[i:i+n] for i in range(0, len(digits), n)]
+    return chunks(digits, width * height)
 
 def countDigitsOnLayer(layer, digit):
     return len(list(filter(lambda d: d == digit, layer)))
+
+def calcPixel(tup):
+    """
+    >>> calcPixel(('2', '0'))
+    '0'
+    >>> calcPixel(('2', '2', '1'))
+    '1'
+    >>> calcPixel(('2', '2'))
+    '2'
+    """
+    rest = list(dropwhile(lambda d: d == '2', tup))
+    return rest[0] if len(rest) > 0 else '2'
+
+def stack(layers):
+    tuples = zip(*layers)
+    return list(map(lambda t: calcPixel(t), tuples))
+
+def renderPixel(px):
+    if px == '2': return ' '
+    if px == '1': return '#'
+    if px == '0': return '.'
+    raise Exception("Unknown: %s" % (px, ))
+
+def renderRow(row):
+    return "".join(list(map(renderPixel, row)))
+
+def render(image, width, height):
+    if len(image) % (width * height) != 0:
+        raise Exception("Unexpected image length")
+    rows = chunks(image, width)
+    return "\n".join(list(map(renderRow, rows)))
 
 def part1():
     """
@@ -22,7 +57,13 @@ def part1():
     numberOf2 = countDigitsOnLayer(layerWithFewest0, '2')
     return numberOf1 * numberOf2
 
+def part2():
+    digits = readInput()
+    layers = toLayers(digits, 25, 6)
+    stacked = stack(layers)
+    return render(stacked, 25, 6)
 
 if __name__ == "__main__":
-    import doctest
-    doctest.testmod()
+    #import doctest
+    #doctest.testmod()
+    print(part2())
