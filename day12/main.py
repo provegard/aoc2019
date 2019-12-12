c7d206f8e1909de57828da3ab164fdcddfec3fac
Per Rovegård <per@rovegard.se>
2019-12-12 13:16:07 +0100

    Day 12 part 2

--- day12/main.py
+++ day12/main.py
@@ -20,7 +20,7 @@ class Moon:
     y_v: int = 0
     z_v: int = 0
 
-    #def copy(self): return Moon(self.x, self.y, self.z, self.v)
+    def copy(self): return Moon(self.x, self.y, self.z, self.x_v, self.y_v, self.z_v)
 
     def updatePosition(self):
         self.x += self.x_v
@@ -54,10 +54,50 @@ def runSteps(moons, steps):
     for _ in range(0, steps):
         timeStep(moons)
 
+def runStepsGen(moons):
+    while True:
+        timeStep(moons)
+        yield moons
+
 def calcTotal(moons, steps):
     runSteps(moons, steps)
     totals = map(lambda m: m.totalEnergy(), moons)
     return sum(totals)
+
+def eq(moons1, moons2, pred):
+    for idx, m1 in enumerate(moons1):
+        m2 = moons2[idx]
+        if not pred(m1, m2):
+            return False
+    return True
+
+def eqX(moons1, moons2): return eq(moons1, moons2, lambda m1, m2: m1.x == m2.x and m1.x_v == m2.x_v)
+def eqY(moons1, moons2): return eq(moons1, moons2, lambda m1, m2: m1.y == m2.y and m1.y_v == m2.y_v)
+def eqZ(moons1, moons2): return eq(moons1, moons2, lambda m1, m2: m1.z == m2.z and m1.z_v == m2.z_v)
+
+def findPeriodicityFor(moons, eqFun):
+    copies = list(map(lambda m: m.copy(), moons))
+    steps = 0
+    for newMoons in runStepsGen(moons):
+        steps += 1
+        if eqFun(newMoons, copies):
+            return steps
+        if steps > 1000000:
+            raise Exception("too far")
+
+def gcd(a,b):
+    while b > 0:
+        a, b = b, a % b
+    return a
+    
+def lcm(a, b):
+    return a * b / gcd(a, b)
+
+def findPeriodicity(moons):
+    x = findPeriodicityFor(moons, lambda ms1, ms2: eqX(ms1, ms2))
+    y = findPeriodicityFor(moons, lambda ms1, ms2: eqY(ms1, ms2))
+    z = findPeriodicityFor(moons, lambda ms1, ms2: eqZ(ms1, ms2))
+    return int(lcm(lcm(x, y), z))
 
 def example1_repr_n(steps):
     """
@@ -88,6 +128,33 @@ def example1_total():
     ]
     return calcTotal(moons, 10)
 
+def example1_steps_return():
+    """
+    >>> example1_steps_return()
+    2772
+    """
+    moons = [
+        Moon(-1, 0, 2),
+        Moon(2, -10, -7),
+        Moon(4, -8, 8),
+        Moon(3, 5, -1)
+    ]
+    return findPeriodicity(moons)
+
+def example2_steps_return():
+    """
+    >>> example2_steps_return()
+    4686774924
+    """
+    moons = [
+        Moon(-8, -10, 0),
+        Moon(5, 5, 10),
+        Moon(2, -7, 3),
+        Moon(9, -8, -3)
+    ]
+    return findPeriodicity(moons)
+
+
 def part1():
     """
     >>> part1()
@@ -100,7 +167,20 @@ def part1():
         Moon(5, -8, 6)
     ]
     return calcTotal(moons, 1000)
-    
+
+def part2():
+    """
+    >>> part2()
+    318382803780324
+    """
+    moons = [
+        Moon(7, 10, 17),
+        Moon(-2, 7, 0),
+        Moon(12, 5, 12),
+        Moon(5, -8, 6)
+    ]
+    return findPeriodicity(moons)
+
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
