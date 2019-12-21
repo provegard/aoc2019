7ed467ca4366d0a34a1d519ac7d325756fa47ace
Per Rovegård <per@rovegard.se>
2019-12-21 22:19:07 +0100

    Day 21 part 2

--- day21/main.py
+++ day21/main.py
@@ -16,15 +16,29 @@ if __name__ == "__main__":
     program = intcode.readProgram("input")
     output = []
 
-    code = [
+    # springdroid jumps 4 tiles (lands 4 tiles from the current one),
+    # i.e. can jump over a hole 3 tiles wide
+    code1 = [
         "NOT C J",
         "AND D J",
         "NOT A T",
         "OR T J",
         "WALK"
-    ]
+    ] # --> 19354818
+    code2 = [
+        "NOT A J",
+        "AND D J",
+        "NOT B T",
+        "AND D T",
+        "OR T J",
+        "NOT C T",
+        "AND D T",
+        "AND H T",
+        "OR T J",
+        "RUN"
+    ] # --> 1143787220
 
-    inputs = toAscii("\n".join(code) + "\n")
+    inputs = toAscii("\n".join(code2) + "\n")
 
     state = intcode.run(program, inputs, output)
-    draw(output) # 19354818
+    draw(output)
