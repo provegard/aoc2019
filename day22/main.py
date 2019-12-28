58689c12cfdb62f9053138e163c8cbc66662759e
Per Rovegård <per@rovegard.se>
2019-12-28 16:28:10 +0100

    Day 22 cleanup

--- day22/main.py
+++ day22/main.py
@@ -5,16 +5,10 @@ import re
 class Deal:
     def apply(self, deck): return deck[::-1]
 
-    def calcAB(self, deckLen, a, b):
-        return (-a, -b - 1)
-
     def desc(self, deckLen, input):
         return "%d-(%s)-1" % (deckLen, input,)
 
     def moveCard(self, deckLen, cardIndex):
-        return deckLen - cardIndex - 1
-
-    def comingFromIdx(self, deckLen, cardIndex):
         return deckLen - cardIndex - 1
 
 @dataclass
@@ -25,12 +19,6 @@ class Cut:
         a = deck[:n]
         b = deck[n:]
         return b + a
-
-    def calcAB(self, deckLen, a, b):
-        n = self.n
-        if n < 0:
-            n = deckLen + n
-        return (a, b - n)
 
     def desc(self, deckLen, inp):
         n = self.n
@@ -48,24 +36,6 @@ class Cut:
         # moves in front
         return cardIndex - n
 
-    def comingFromIdx(self, deckLen, cardIndex):
-        n = self.n
-        moveLen = abs(n)
-        stationaryLen = deckLen - moveLen
-        if n > 0:
-            # n cards were moved from the front
-            if cardIndex < stationaryLen:
-                # card wasn't one that moved
-                return cardIndex + moveLen
-            # card did move
-            return cardIndex - stationaryLen
-        # n cards were moved from the back
-        if cardIndex < moveLen:
-            # the card did move
-            return cardIndex + stationaryLen
-        # the card didn't move
-        return cardIndex - moveLen
-
 @dataclass
 class DealIncrement:
     n: int
@@ -77,10 +47,6 @@ class DealIncrement:
             space[pos] = card
         return space
 
-    def calcAB(self, deckLen, a, b):
-        n = self.n
-        return (a * n, b * n)
-
     def moveCard(self, deckLen, cardIndex):
         n = self.n
         #return (cardIndex * n) % deckLen
@@ -89,18 +55,6 @@ class DealIncrement:
     def desc(self, deckLen, input):
         n = self.n
         return "(%s)*%d" % (input, n,)
-
-    def comingFromIdx(self, deckLen, cardIndex):
-        # (N * idx) mod L = new_idx
-        # N * idx = yL + new_idx
-        # N * idx - newIdx = yL
-        # (N * idx - newIdx) / L = y
-        n = self.n
-        for y in range(0, n):
-            c = (y * deckLen) + cardIndex
-            if c % n == 0:
-                return c // n
-        raise Exception(":( for %d, %d" % (deckLen, cardIndex))
 
 def parse(line):
     """
@@ -137,31 +91,6 @@ def testApply(move, deck):
     """
     return move.apply(deck)
 
-def testComingFromIdx(move, deckLen, idx):
-    """
-    >>> testComingFromIdx(Deal(), 5, 1)
-    3
-    >>> testComingFromIdx(Cut(2), 5, 4)
-    1
-    >>> testComingFromIdx(Cut(2), 5, 0)
-    2
-    >>> testComingFromIdx(Cut(-2), 6, 4)
-    2
-    >>> testComingFromIdx(Cut(-2), 6, 2)
-    0
-    >>> testComingFromIdx(Cut(-2), 6, 1)
-    5
-    >>> testComingFromIdx(DealIncrement(3), 10, 1)
-    7
-    >>> testComingFromIdx(DealIncrement(3), 10, 4)
-    8
-    >>> testComingFromIdx(DealIncrement(3), 10, 3)
-    1
-    >>> testComingFromIdx(DealIncrement(52), 10119315717514047, 2020)
-    2335226704041742
-    """
-    return move.comingFromIdx(deckLen, idx)
-
 def testMoveCard(move, deckLen, idx):
     """
     >>> testMoveCard(DealIncrement(52), 10119315717514047, 2335226704041742)
@@ -179,7 +108,7 @@ def part1():
     >>> part1()
     1510
     """
-    moves = readInput()
+    #moves = readInput()
     cardIndex = 2019
     deckLen = 10007
 
@@ -198,167 +127,54 @@ def part1():
     #    expr = move.desc(deckLen, expr)
     #print(expr)
 
-def reverse(lst): return lst[::-1]
+# from the Internet
+def linear_congruence(a, b, m):
+    if b == 0:
+        return 0
 
-def executeMoves(deckLen, moves, idx):
-    for move in moves:
-        idx = move.moveCard(deckLen, idx) % deckLen
-    return idx
+    if a < 0:
+        a = -a
+        b = -b
 
-def findCycle(deckLen, moves, oneRep):
-    idx0 = 0
-    idx1 = 1
-    idx2 = 2
-    rep = 0
-    if oneRep is None:
-        oneRep = lambda i:executeMoves(deckLen, moves, i)
-    while rep < 200000000:
-        idx0 = oneRep(idx0)
-        idx1 = oneRep(idx1)
-        idx2 = oneRep(idx2)
-        rep += 1
-        if idx1 == idx0 + 1 and idx2 == idx1 + 1:
-            return rep
-        if rep % 10000 == 0:
-            print(rep)
-    raise Exception("Didn't find a cycle")
+    b %= m
+    while a > m:
+        a -= m
 
+    return (m * linear_congruence(m, -b, a) + b) // a
 
-def part2x():
-    moves = readInput()
-    # deckLen = 983     # 982    (n-1)      # 982: 2 x 491
-    # deckLen = 991     # 990    (n-1)
-    # deckLen = 997     # 996    (n-1)
-    # deckLen = 1009    # 336    (n-1)/3    # 1008: 2 x 2 x 2 x 2 x 3 x 3 x 7
-    # deckLen = 1013    # 253    (n-1)/4    # 1012: 2 x 2 x 11 x 23
-    # deckLen = 1019    # 509    (n-1)/2    # 1018: 2 x 509
-    # deckLen = 1021    # 68     (n-1)/15   # 1020: 2 x 2 x 3 x 5 x 17
-    # deckLen = 1039    # 346    (n-1)/3    # 1038: 2 x 3 x 173
-    # deckLen = 1049    # 1048   (n-1)   
-    # deckLen = 1997    # 499    (n-1)/4
-    # deckLen = 1999    # 999    (n-1)/2
-    # deckLen = 2927    # 1463   (n-1)/2
-    # deckLen = 10007   # 5003   (n-1)/2
-    # deckLen = 13997   # 6998   (n-1)/2     
-    # deckLen = 19997   # 19996  (n-1)
-    # deckLen = 30011   # 15005  (n-1)/2
-    # deckLen = 97777   # 97776  (n-1)
-    # deckLen = 97787   # 97786  (n-1)
-    # deckLen = 100019  # 100018 (n-1)
-    # deckLen = 1299827 # 649913 (n-1)/2
+def part2():
+    import math
+    import functools
 
-    p = -2370828689882754717559498128567964034494557509910528000000
-    q = 39498975727710714403582787330038714109202045914535069409110464016
-    nextIdx = lambda i:(p*i+q) % deckLen
-    deckLen     = 119315717514047 # prime factors of 119315717514046: 2 × 59657858757023
+    deckLen     = 119315717514047
     repetitions = 101741582076661
-    print(findCycle(deckLen, moves, nextIdx))
-
-    idx = 0
-    #for move in moves:
-    #    idx = move.moveCard(deckLen, idx) % deckLen
-    #print(deckLen / repetitions)
-
-    # rep = 0
-    # start = 0
-    # i = start
-    # while True:
-    #     for move in moves:
-    #         i = move.moveCard(deckLen, i) % deckLen
-    #     rep += 1
-    #     if i == start:
-    #         raise Exception("back to i==%d after %d reps" % (start, rep,))
-    #     if rep % 100 == 0:
-    #         print(rep)
 
     # 1. Create the shuffle expression
-    # deckLen = 1021
     # expr = "I"
     # for move in moves:
     #    expr = move.desc(deckLen, expr)
     # print(expr)
 
     # 2. Simplify via # https://www.dcode.fr/math-simplification:
-    # -39216*(60455647946826670684401727064666565547086839808000000*i-1007215823330036576998745086955291567452112553920212908229051)
-    # a*(b*i+c) => a*b*i + a*c, set p=a*b and q=a*c:
-    
-    #print(findCycle(deckLen, moves, nextIdx))
-
-        
-    # ---> 649913 reps
-    # 101741582076661 % 649913 = 19029
-
-    # 927987 är för lågt!!
-    # 98130591375625 är fel (ger 2020 efter rep 19028)
-
-    # 11084257945798 då? (ger 2020 efter rep 19029)
-    # 11084257945798 OCKSÅ FEL!!
-
-    # 56052654109315 är fel, ett steg bakåt från 2020
-
-    # idx = 2020
-    # revMoves = reverse(moves)
-    # for _ in range(0, 1):
-    #     nxt = idx
-    #     for move in revMoves:
-    #         idx = move.comingFromIdx(deckLen, idx)
-    #     calcNxt = nextIdx(idx)
-    #     assert(nxt == calcNxt)
-    # print(idx)
-
-
-    start = 11084257945798
-    idx = start
-    rep = 0
-    # while True:
-    #     idx = nextIdx(idx)
-    #     rep += 1
-    #     if idx == start:
-    #         raise Exception("back to idx==%d after %d reps" % (start, rep,))
-    #     if rep % 10000 == 0:
-    #         print(rep)
-    # while rep < 19100:
-    #     idx = nextIdx(idx)
-    #     rep += 1
-    #     if rep >= 19000:
-    #         print("rep %d -> %d" % (rep, idx))
-    # print(idx)
-    # idx = 2020
-    # lastDiff = 0
-    # for x in range(0, 5):
-    #     #print(idx)
-    #     ni = nextIdx(idx)
-    #     diff = ni - idx
-    #     print("==")
-    #     print(diff // p) # 0
-    #     lastDiff = diff
-    #     idx = ni
-
-
-    #fwd = lambda n, i:((p**n)*i + q*((p**n)-1)) % deckLen
-
-    #print(fwd(repetitions, 2020))
-
-def part2():
-    import functools
-
-    deckLen     = 119315717514047
-    repetitions = 101741582076661
-
+    # to get p and q in p*i+q:
     p = -2370828689882754717559498128567964034494557509910528000000 % deckLen
     q = 39498975727710714403582787330038714109202045914535069409110464016 % deckLen
     
+    # one complete shuffle
     shuffle1 = (p, q)
     
+    # combine two shuffles
     def newShuffle(s1, s2):
         a, b = s1
         c, d = s2
         return (a * c % deckLen, (b * c + d) % deckLen)
 
+    # apply a shuffle
     def doShuffle(s, idx):
         a, b = s
         return (idx * a + b) % deckLen
 
+    # combine shuffles so that we do 2^n individual shuffles, return the shuffle + n
     def findShuffle(maxRep):
         n = 1
         s = shuffle1
@@ -366,7 +182,8 @@ def part2():
             s = newShuffle(s, s)
             n *= 2
         return (s, n)
-    
+
+    # create a list of shuffles so that we reach 'repetitions' shuffles in total
     maxRep = repetitions
     shuffles = []
     while maxRep > 0:
@@ -374,38 +191,27 @@ def part2():
         shuffles.append(s)
         maxRep -= n
 
+    # simplify into a single big shuffle
     finalShuffle = functools.reduce(newShuffle, shuffles)
     a, b = finalShuffle
     #print(finalShuffle)
 
+    # The reverse of the final shuffle, where 'q' is some integer so that dividing by 'a'
+    # yields a whole number:
     # reverse: i = (q*deckLen + x - b) / a
     # - x = 2020
     # - q = ????
 
-    def linear_congruence(a, b, m):
-        if b == 0:
-            return 0
-
-        if a < 0:
-            a = -a
-            b = -b
-
-        b %= m
-        while a > m:
-            a -= m
-
-        return (m * linear_congruence(m, -b, a) + b) // a
-
     # (q * deckLen + x - b) = 0 (mod a)
-    # q * deckLen = (b-x) mod a ??
-
-    #print(q) # 3396925699541
+    # q * deckLen = (b-x) mod a
     def revIdx(x):
         q = linear_congruence(deckLen, b-x, a)
-        return ((q*deckLen + x - b) / a) % deckLen
+        return math.floor(((q*deckLen + x - b) / a) % deckLen)
 
-    print(revIdx(2020)) # 10307144922975
-    print(doShuffle(finalShuffle, 10307144922975))
+    answer = revIdx(2020)
+    print("answer  = %d" % (answer,)) # 10307144922975
+    control = doShuffle(finalShuffle, answer)
+    print("control = %d" % (control,))
     # -> 10307144922975
 
 if __name__ == "__main__":
